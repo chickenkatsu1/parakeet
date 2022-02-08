aecf612cf89b8f2b0530f551342f55c41ad72f80
acxz <17132214+acxz@users.noreply.github.com>
2022-02-07 20:08:14 -0500

    use single quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Posts from './components/Posts';
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <div classame="App">
+    <div classame='App'>
       <NavBar></NavBar>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Posts></Posts>} />
+        <Route path='/' element={<Posts></Posts>} />
       </Routes>
       <Routes>
-        <Route path="/:subredditName" element={<Posts></Posts>} />
+        <Route path='/:subredditName' element={<Posts></Posts>} />
       </Routes>
       <Routes>
-        <Route path="/:subredditName/:categoryName" element={<Posts></Posts>} />
+        <Route path='/:subredditName/:categoryName' element={<Posts></Posts>} />
       </Routes>
       <Routes>
-        <Route path="/:subredditName/:categoryName/:time" element={<Posts></Posts>} />
+        <Route path='/:subredditName/:categoryName/:time' element={<Posts></Posts>} />
       </Routes>
       </BrowserRouter>
     </div>
