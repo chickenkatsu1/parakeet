0eeba2964c212eb0cb9b3e4d7a94914e990c80c6
chickenkatsu1 <prabhathc88@gmail.com>
2022-02-07 22:41:12 -0500

    replaced masonry library with mui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Posts from './components/Posts';
+import Board from './components/Board';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -10,16 +10,16 @@ function App() {
       <NavBar></NavBar>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Posts></Posts>} />
+        <Route path='/' element={<Board></Board>} />
       </Routes>
       <Routes>
-        <Route path='/:subredditName' element={<Posts></Posts>} />
+        <Route path='/:subredditName' element={<Board></Board>} />
       </Routes>
       <Routes>
-        <Route path='/:subredditName/:categoryName' element={<Posts></Posts>} />
+        <Route path='/:subredditName/:categoryName' element={<Board></Board>} />
       </Routes>
       <Routes>
-        <Route path='/:subredditName/:categoryName/:timeName' element={<Posts></Posts>} />
+        <Route path='/:subredditName/:categoryName/:timeName' element={<Board></Board>} />
       </Routes>
       </BrowserRouter>
     </div>
