2be1fb6cde5ea328e839756c04b1fa40434dccd7
mloo3 <14188465+mloo3@users.noreply.github.com>
2022-02-07 20:35:34 -0800

    Update index.js

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,46 +1,84 @@
 import Post from '../Post';
-import Masonry from '@mui/lab/Masonry';
+import Masonry from 'react-masonry-component';
 import { useEffect, useState } from 'react';
 import Snoowrap from 'snoowrap';
 import env from '../../env.config.json';
 import { useParams } from 'react-router-dom';
 
-const Board = () => {
+const Posts = ({stateChanger}) => {
+
     let params = useParams();
     let subredditName = params.subredditName ? params.subredditName: 'analog';
     let categoryName = params.categoryName ? params.categoryName: 'hot';
     let timeName = params.timeName ? params.timeName: 'day';
 
-    const [allPosts, setPosts] = useState([]);
+    const masonryOptions = {
+        stagger: 0,
+        transitionDuration: '0.3s',
+        percentPosition: true,
+    }
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const [pageNum, setCount] = useState(0);
+    const [allPosts, setPosts] = useState([])
+    const [bookmark, setBookmark] = useState(0)
+    const [authorName, setAuthName] = useState('')
+
+    
+    async function handleScroll(e) {
+        console.log("LKASJDKLJASD")
+        if (Math.abs(e.target.scrollHeight - (e.target.scrollTop + e.target.clientHeight)) <= 1) {
+            setCount(pageNum++)
+        } else {
+            console.log("LKASJDKLJASD")
+        }
+
+        // const bottom = Math.abs(e.target.scrollHeight - (Math.ceil(e.target.scrollTop) === e.target.clientHeight))
+        // if (bottom) {
+        //     setCount(pageNum++)
+        // }
+    }
 
     async function getPosts() {
+        console.log('UPDATE')
         const postRequestLimit = 25;
         // reddit rate limit: 60/min
         // Source: https://github.com/reddit-archive/reddit/wiki/API#rules
 
-        let posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
+        let params = {limit: postRequestLimit}
+        if (pageNum != 0) {
+            params.count = bookmark
+            params.after = authorName
+        }
+
+        var posts
 
         switch (categoryName) {
             case 'new':
-                posts = (await r.getSubreddit(subredditName).getNew({limit: postRequestLimit}));
+                console.log("YOU HERE?")
+                posts = (await r.getSubreddit(subredditName).getNew(params));
                 break;
             case 'top':
-                posts = (await r.getSubreddit(subredditName).getTop({time: timeName, limit: postRequestLimit}));
+                params.time = timeName
+                posts = (await r.getSubreddit(subredditName).getTop(params));
                 break;
             case 'rising':
-                posts = (await r.getSubreddit(subredditName).getRising({limit: postRequestLimit}));
+                posts = (await r.getSubreddit(subredditName).getRising(params));
                 break;
             default:
                 // getHot always returns 2 more than specified (bug in snoowrap?)
-                posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
+                posts = (await r.getSubreddit(subredditName).getHot(params));
         }
 
+        setAuthName(posts[posts.length - 1].name)
+        setBookmark(bookmark + posts.length)
         setPosts(posts);
     }
 
     useEffect(() => {
         getPosts();
-    }, [])
+    }, [pageNum])
 
     const r = new Snoowrap({
         userAgent: env.userAgent,
@@ -59,12 +97,16 @@ const Board = () => {
         })
     }
     return (
-        <Masonry columns={4} spacing={1}>
-            {
-                getImages()
-            }
-        </Masonry>
+        <div onScroll={console.log("AKLSJD")}>
+            <Masonry
+                options={masonryOptions}
+            >
+                {
+                    getImages()
+                }
+            </Masonry>
+        </div>
     )
 }
 
-export default Board;
+export default Posts;
