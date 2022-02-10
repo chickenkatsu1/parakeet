a90d1d4edede8b5571dfa51c5fc22eb25fb6260a
acxz <17132214+acxz@users.noreply.github.com>
2022-02-10 16:16:12 -0500

    fix various warnings

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,7 +7,7 @@ import { useParams, useSearchParams } from 'react-router-dom';
 
 const Board = () => {
     let params = useParams();
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
 
     let subredditName = params.subredditName ? params.subredditName: 'analog';
     let categoryName = params.categoryName ? params.categoryName: 'hot';
@@ -15,41 +15,41 @@ const Board = () => {
 
     const [allPosts, setPosts] = useState([]);
 
-    async function getPosts() {
-        const postRequestLimit = 25;
-        // reddit rate limit: 60/min
-        // Source: https://github.com/reddit-archive/reddit/wiki/API#rules
-
-        let posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
-
-        switch (categoryName) {
-            case 'new':
-                posts = (await r.getSubreddit(subredditName).getNew({limit: postRequestLimit}));
-                break;
-            case 'top':
-                posts = (await r.getSubreddit(subredditName).getTop({time: timeName, limit: postRequestLimit}));
-                break;
-            case 'rising':
-                posts = (await r.getSubreddit(subredditName).getRising({limit: postRequestLimit}));
-                break;
-            default:
-                // getHot always returns 2 more than specified (bug in snoowrap?)
-                posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
-        }
-
-        setPosts(posts);
-    }
-
     useEffect(() => {
-        getPosts();
-    }, [])
 
-    const r = new Snoowrap({
-        userAgent: env.userAgent,
-        clientId: env.clientId,
-        clientSecret: env.clientSecret,
-        refreshToken: env.refreshToken
-    });
+        const r = new Snoowrap({
+            userAgent: env.userAgent,
+            clientId: env.clientId,
+            clientSecret: env.clientSecret,
+            refreshToken: env.refreshToken
+        });
+
+        async function getPosts() {
+            const postRequestLimit = 25;
+            // reddit rate limit: 60/min
+            // Source: https://github.com/reddit-archive/reddit/wiki/API#rules
+
+            let posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
+
+            switch (categoryName) {
+                case 'new':
+                    posts = (await r.getSubreddit(subredditName).getNew({limit: postRequestLimit}));
+                    break;
+                case 'top':
+                    posts = (await r.getSubreddit(subredditName).getTop({time: timeName, limit: postRequestLimit}));
+                    break;
+                case 'rising':
+                    posts = (await r.getSubreddit(subredditName).getRising({limit: postRequestLimit}));
+                    break;
+                default:
+                    // getHot always returns 2 more than specified (bug in snoowrap?)
+                    posts = (await r.getSubreddit(subredditName).getHot({limit: postRequestLimit}));
+            }
+
+            setPosts(posts);
+        }
+        getPosts();
+    }, [categoryName, subredditName, timeName])
 
     const getImages = () => {
         return allPosts.map((post) => {
@@ -57,7 +57,8 @@ const Board = () => {
                 return Object.keys(post.media_metadata).map((obj) =>  (<Post key={post.media_metadata[obj].id} media={post.media_metadata[obj].s.u} thumbnail={post.thumbnail} post={post}></Post>));
             } else if (post.url_overridden_by_dest && !post.is_video) {
                 return (<Post key={post.id} media={post.url_overridden_by_dest} thumbnail={post.thumbnail} post={post}></Post>)
-            }
+            } else
+                return null;
         })
     }
     return (
