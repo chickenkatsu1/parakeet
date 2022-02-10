4c282ad589de8aed888982d0416f4ecfd3003b08
acxz <17132214+acxz@users.noreply.github.com>
2022-02-10 16:16:12 -0500

    fix various warnings

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,7 @@
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">analog</a>
+            <a className="navbar-brand" href="/">analog</a>
         </nav>
     )
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ProgressiveImage from 'react-progressive-image';
-import ImgurEmbed from 'react-imgur-embed';
 
 const Post = ({media, thumbnail}) => {
     // console.log(link, post);
@@ -8,7 +7,7 @@ const Post = ({media, thumbnail}) => {
         <ProgressiveImage 
                 src={media} placeholder={thumbnail}
             >
-                {(src, loading) => <img style={{height: 'auto', width: '33vw', opacity: loading ? 0 : 1}} src={src} alt="an image" />}
+                {(src, loading) => <img style={{height: 'auto', width: '33vw', opacity: loading ? 0 : 1}} src={src} alt="" />}
             </ProgressiveImage>
     )
 }
