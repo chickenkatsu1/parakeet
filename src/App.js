806fd55cc933a33354102123ded988c986493ee3
acxz <17132214+acxz@users.noreply.github.com>
2022-02-12 13:18:44 -0500

    handle incorrect urls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Board from './components/Board';
+import NoMatch from './components/NoMatch';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -11,17 +12,13 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Board></Board>} />
-      </Routes>
-      <Routes>
         <Route path='/r/:subredditName' element={<Board></Board>} />
-      </Routes>
-      <Routes>
         <Route path='/r/:subredditName/:categoryName' element={<Board></Board>} />
+        <Route path='*' element={<NoMatch></NoMatch>} />
       </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/NoMatch/index.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+function NoMatch() {
+    // 404
+    return (
+      <div>
+        <h2>Nothing to see here!</h2>
+        <p>
+          <Link to="/">Go to the home page</Link>
+        </p>
+      </div>
+    );
+}
+
+export default NoMatch
